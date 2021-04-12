78a37e50de5461694cb488785664066949429e23
bernardasc@hotmail.com <bernardasc@hotmail.com>
2021-04-12 10:23:40 +0300

    multi gpu, train clsf

--- IsVisible_MakeModel/train_gpu1.py
+++ IsVisible_MakeModel/train_gpu1.py
@@ -6,5 +6,5 @@ gpu_id = 1
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]=str(gpu_id)
 
-for i in range(1):
+for i in range(5):
     model_clsf = train_multi_versions_single_gpu.train_on_single_gpu (gpu_id=gpu_id)
