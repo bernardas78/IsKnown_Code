8c299f8f25bcb164cd67fe858a56d75e8f50a516
bernardasc@hotmail.com <bernardasc@hotmail.com>
2021-04-12 10:12:52 +0300

    multi gpu, train clsf

--- Autoenc/train_multi_gpu2.py
+++ Autoenc/train_multi_gpu2.py
@@ -5,7 +5,7 @@ os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 clsf_from_autoenc_params = {
-    "fc_version": 2,
+    "fc_version": 1,
     "autoenc_version": 2,
     "autoenc_datetrained": "20210409"
     #"autoenc_filename": "model_autoenc_20210409_v2.h5"
