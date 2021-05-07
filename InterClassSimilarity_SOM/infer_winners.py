afcd00ce260d008e244e92e7329a271e58f0b443
bernardasc@hotmail.com <bernardasc@hotmail.com>
2021-05-07 14:28:38 +0300

    multi gpu, train clsf

--- InterClassSimilarity_SOM/infer_winners.py
+++ InterClassSimilarity_SOM/infer_winners.py
@@ -11,6 +11,10 @@ import time
 
 dim_size = 14
 
+set_names = ["Train", "Val", "Test"]
+do_predict = False
+do_piecharts = True
+
 # SOM clusterer
 som_filename = os.path.join(Glb.results_folder, "som_clusterer_{}x{}".format(dim_size, dim_size) )
 
@@ -20,20 +24,22 @@ clusters_filename_pattern = "som_clstrs_{}_{}x{}.h5"
 # Load SOM clusterer
 mysom = pickle.load ( open(som_filename, 'rb') )
 
-for set_name in ["Train", "Val", "Test"]:
-    # Load activations
-    orange_tab = loadActivations(set_name)
+for set_name in set_names:
+    if do_predict:
+        # Load activations
+        orange_tab = loadActivations(set_name)
 
-    # Inference: get winner neurons (i.e. cluster asssignment) for each sample
-    now = time.time()
-    pred_winner_neurons = mysom.winners ( orange_tab.X )
-    print("Prdicted winners in {} seconds".format(time.time() - now))
+        # Inference: get winner neurons (i.e. cluster asssignment) for each sample
+        now = time.time()
+        pred_winner_neurons = mysom.winners ( orange_tab.X )
+        print("Prdicted winners in {} seconds".format(time.time() - now))
 
-    # Save cluster assignment files
-    clusters_filename = os.path.join ( Glb.results_folder, clusters_filename_pattern.format ( set_name, str(dim_size), str(dim_size) ) )
-    now = time.time()
-    pickle.dump( (pred_winner_neurons, orange_tab.Y), open(clusters_filename, 'wb'))
-    print("Saved winners in {} seconds".format(time.time() - now))
+        # Save cluster assignment files
+        clusters_filename = os.path.join ( Glb.results_folder, clusters_filename_pattern.format ( set_name, str(dim_size), str(dim_size) ) )
+        now = time.time()
+        pickle.dump( (pred_winner_neurons, orange_tab.Y), open(clusters_filename, 'wb'))
+        print("Saved winners in {} seconds".format(time.time() - now))
 
-    # Calculate purity and draw pie charts
-    purity_pie(set_name,dim_size)
+    if do_piecharts:
+        # Calculate purity and draw pie charts
+        purity_pie(set_name,dim_size)
--- InterClassSimilarity_SOM/pie_cluster_purity.py
+++ InterClassSimilarity_SOM/pie_cluster_purity.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 from collections import Counter
 from Globals.globalvars import Glb
+import pandas as pd
 
 import matplotlib
 matplotlib.use('Agg')   # otherwise, on 1080 fails importing pyplot
@@ -17,6 +18,7 @@ clusters_filename_pattern = "som_clstrs_{}_{}x{}.h5"
 #dim_size = 14
 
 def purity_pie(set_name, dim_size):
+    df_prodnames = pd.read_csv("df_prods_194.csv", header=0)["product"].tolist()
     clusters_filename = os.path.join ( Glb.results_folder, clusters_filename_pattern.format ( set_name, str(dim_size), str(dim_size) ) )
 
     (pred_winner_neurons, lbls) = pickle.load( open(clusters_filename, 'rb') )
@@ -30,14 +32,27 @@ def purity_pie(set_name, dim_size):
             this_neuron_lbls = lbls[(pred_winner_neurons[:, 0] == i) & (pred_winner_neurons[:, 1] == j)].astype(int)
             most_common_classes = Counter(this_neuron_lbls).most_common()
             if len(most_common_classes) > 0:
-                max_cnt = np.minimum(len(most_common_classes),5)
+                # top 80% containing classes
+                cum_probs = np.cumsum([item[1]/len(this_neuron_lbls) for item in most_common_classes])
+                max_cnt = np.where(cum_probs > 0.8)[0][0] + 1
+                max_cnt = np.minimum(max_cnt,5)
+                #print ("i={},j={},max_cnt={}".format(i,j,max_cnt))
+                #if max_cnt==0:
+                #    print (cum_probs[:4])
+
                 sizes_first_n = [item[1] for item in most_common_classes[:max_cnt]]
                 sizes = sizes_first_n + [len(this_neuron_lbls) - np.sum(sizes_first_n)] # Add "Other"
-                labels = [item[0] for item in most_common_classes[:max_cnt]] + ["Other"]
+                prod_indices = [ item[0] for item in most_common_classes[:max_cnt]]
+                labels = [ df_prodnames[ind] for ind in prod_indices] + [""] # Add "Other"
+                labels = [ lbl[:10] for lbl in labels ] # left10
 
                 #print ("i={}, j={}, Len={}".format(i,j,len(this_neuron_lbls)))
                 radius = np.sqrt( len(this_neuron_lbls) / len(lbls)) * dim_size
-                axes[i,j].pie(sizes, labels=labels, colors=colors, radius=radius)#, textprops={'fontsize': 5}) #, autopct='%1.1f%%')
+                axes[i,j].pie(sizes, labels=labels, colors=colors, radius=radius, textprops={'fontsize': 4})#, textprops={'fontsize': 5}) #, autopct='%1.1f%%')
+            else:
+                #axes[i, j].set_xticks([])
+                #axes[i, j].set_yticks([])
+                axes[i, j].axis('off')
 
     #plt.show()
     plt.savefig(purity_filename_pattern.format(set_name, dim_size,dim_size))
