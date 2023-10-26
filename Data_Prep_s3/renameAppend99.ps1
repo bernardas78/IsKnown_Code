37e9e2d621dfe7178d8f48b73a448e6de766dbfe
bernardas78 <bernardasc@hotmail.com>
2023-10-26 10:55:19 +0300

    *

---
+++ Data_Prep_s3/renameAppend99.ps1
@@ -0,0 +1,11 @@
+﻿$root_folder = 'c:\IsKnown_Images_IsVisible\Val'
+
+del $out_csv
+Get-ChildItem $root_folder |
+ForEach-Object {
+    #$newname = $_.Parent.FullName + "\" + "99" + $_.Name + "00000"
+    $newname = $_.Parent.FullName + "\" + "99" + $_.Name.Substring(70,5)+ "00000"
+    echo  $_.FullName
+    echo  $newname
+    Rename-Item $_.FullName $newname
+}
